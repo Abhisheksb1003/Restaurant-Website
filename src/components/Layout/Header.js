4249fd91624b70f0bdf2412199c4dcc7ca4088a7
Abhisheksb1003 <abhishekbikkannavar1@gmail.com>
2023-11-16 15:06:19 +0530

    Restaurant

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+import classes from './Header.module.css';
+import mealsImage from '../../assets/Mealimage.jpg';
+import Headercartbutton from "./Headercartbutton";
+
+const Header=(props)=>{
+   return (<Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <Headercartbutton />
+            
+        </header>
+        <div className={classes["main-image"]}>
+            <img src={mealsImage} alt="Food Image" />
+        </div>
+    </Fragment>)
+}
+
+export default Header
---
+++ src/components/Layout/Headercartbutton.js
@@ -0,0 +1,18 @@
+import classes from './Headercartbutton.module.css';
+import React from 'react';
+import CartIcon from '../Cart/CartIcon';
+
+const Headercartbutton=()=>{
+    return (
+        <button className={classes.button}>
+            <span className={classes.icon}>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span className={classes.badge}>1</span>
+        </button>
+    )
+
+}
+
+export default Headercartbutton
